648f4f7e910cb163450cf50c0f4902bcdaa42191
Yaqoob <sualehfarooq65@gmail.com>
2023-01-21 12:56:48 +0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app>
-      <v-sheet color="grey lighten-4" class="pa-4">
+      <v-sheet color="blue lighten-4" class="pa-4">
         <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
 
         <div>john@vuetifyjs.com</div>
@@ -33,6 +33,7 @@
         <v-list-item v-for="[icon, text] in links" :key="icon" link>
           <v-list-item-icon>
             <v-icon>{{ icon }}</v-icon>
+            
           </v-list-item-icon>
 
           <v-list-item-content>
