57a2d3e44f776fc7b1fe507211458dde82c3c532
Yaqoob <sualehfarooq65@gmail.com>
2023-01-21 13:15:01 +0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <v-app id="inspire">
     <v-app-bar app absolute color="yellow">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
       <v-toolbar-title>Title</v-toolbar-title>
 
       <v-spacer></v-spacer>
@@ -10,47 +9,42 @@
       <v-btn @click="show" icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-
       <v-btn icon>
         <v-icon>mdi-heart</v-icon>
       </v-btn>
-
       <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app >
       <v-sheet color="blue lighten-4" class="pa-4">
         <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
-
         <div>john@vuetifyjs.com</div>
       </v-sheet>
-
       <v-divider></v-divider>
-
-      <v-list>
-        <v-list-item v-for="[icon, text] in links" :key="icon" link>
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
-            
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+      <!-- Navigation list in side drawer-->
+      <v-list dense>
+        <v-subheader>REPORTS</v-subheader>
+        <v-list-item-group v-model="selectedItem" color="warning">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item> </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-  
-<Inbox />
-    <!-- <v-alert elevation="8" type="success" dismissible>Alert Message</v-alert> -->
-  
+
   </v-app>
 </template>
 
 <script>
-import Inbox from './views/Inbox.vue';
+import Inbox from "./views/Inbox.vue";
 export default {
   data: () => ({
     cards: ["Today", "Yesterday"],
@@ -61,46 +55,25 @@ export default {
       ["mdi-delete", "Trash"],
       ["mdi-alert-octagon", "Spam"],
     ],
-    
+    selectedItem: 2,
+    items: [
+      { text: "Real-Time", icon: "mdi-clock" },
+      { text: "Audience", icon: "mdi-account" },
+      { text: "Conversions", icon: "mdi-flag" },
+    ],
   }),
-  components : {
-    Inbox
+  components: {
+    Inbox,
   },
-  methods  : {
+  methods: {
     show() {
       this.$swal.fire(
-  'The Internet?',
-  'That thing is still around?',
-  'warning'
-)
-    }
+        "The Internet?",
+        "That thing is still around?",
+        "warning"
+      );
+    },
   },
-  
 };
 </script>
 
-<!-- 
-<template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-    </v-main>
-  </v-app>
-</template>
-
-<script>
-  export default {
-    data: () => ({ drawer: null }),
-  }
-</script> -->
