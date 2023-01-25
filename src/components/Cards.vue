bd9a3ed1dad3fddaf4557964209074966bc89fc6
Yaqoob <sualehfarooq65@gmail.com>
2023-01-25 16:06:59 +0500

    commit

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,46 +1,186 @@
 <template>
-<v-main >
-      <v-container >
-        <div class="main_div">
-        <v-row class="chart_upper_row"  >
-          <v-col
-            v-for="n in 1"
-            :key="n" >
-            <v-card class="mx auto" max-width="344" outlined> <apexchart width="100%" height="100%" type="bar" :options="chartOptions" :series="series"></apexchart></v-card>
-            <v-card class="mx auto" max-width="344" outlined> <apexchart width="100%" height="100%" type="bar" :options="chartOptions" :series="series"></apexchart></v-card>
-            <v-card class="mx auto" max-width="344" outlined> <apexchart width="100%" height="100%" type="bar" :options="chartOptions" :series="series"></apexchart></v-card>
-
-
-             </v-col>
-            </v-row>
-          </div>
-      </v-container>
-    </v-main>
+  <v-main>
+    <v-container>
+      <div class="main_div">
+        <v-row class="chart_upper_row">
+          <v-col v-for="n in 1" :key="n">
+            <v-card class="mx auto" max-width="344" outlined
+              ><apexchart
+                type="line"
+                height="100%"
+                width="100%"
+                :options="chartOptionssecond"
+                :series="seriessecond"
+              ></apexchart
+            ></v-card>
+            <v-card class="mx auto" max-width="344" outlined
+              ><apexchart
+                type="area"
+                height="100%"
+                width="100%"
+                :options="chartOptions"
+                :series="series"
+              ></apexchart
+            ></v-card>
+            <v-card class="mx auto" max-width="344" outlined
+              ><apexchart
+                type="bar"
+                height="100%"
+                width="100%"
+                :options="chartOptionssecond"
+                :series="seriessecond"
+              ></apexchart
+            ></v-card>
+            
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
 export default {
-    name : 'CompChrts',
-    data: function() {
-      return {
-        chartOptions: {
-          chart: {
-            id: 'vuechart-example'
+  name: "CompChrts",
+  data : ()=>({
+    series: [{
+              name: "Desktops",
+              data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+          }],
+          chartOptions: {
+            chart: {
+              height: 350,
+              type: 'line',
+              zoom: {
+                enabled: false
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              curve: 'straight'
+            },
+       
+            xaxis: {
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+            }
           },
-          xaxis: {
-            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-          }
-        },
-        series: [{
-          name: 'series-1',
-          data: [30, 40, 35, 50, 49, 60, 70, 91]
-        }]
-      }
-    },
-}
+
+          seriessecond: [{
+            name: 'series1',
+            data: [31, 40, 28, 51, 42, 109, 100]
+          }, {
+            name: 'series2',
+            data: [11, 32, 45, 32, 34, 52, 41]
+          }],
+          chartOptionssecond: {
+            chart: {
+              height: 350,
+              type: 'area'
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              curve: 'smooth'
+            },
+            xaxis: {
+              type: 'datetime',
+              categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+            },
+            tooltip: {
+              x: {
+                format: 'dd/MM/yy HH:mm'
+              },
+            },
+          },
+
+
+          seriesthird: [{
+            name: 'Inflation',
+            data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+          }],
+          chartOptionsthird: {
+            chart: {
+              height: 350,
+              type: 'bar',
+            },
+            plotOptions: {
+              bar: {
+                borderRadius: 10,
+                dataLabels: {
+                  position: 'top', // top, center, bottom
+                },
+              }
+            },
+            dataLabels: {
+              enabled: true,
+              formatter: function (val) {
+                return val + "%";
+              },
+              offsetY: -20,
+              style: {
+                fontSize: '12px',
+                colors: ["#304758"]
+              }
+            },
+            
+            xaxis: {
+              categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+              position: 'top',
+              axisBorder: {
+                show: false
+              },
+              axisTicks: {
+                show: false
+              },
+              crosshairs: {
+                fill: {
+                  type: 'gradient',
+                  gradient: {
+                    colorFrom: '#D8E3F0',
+                    colorTo: '#BED1E6',
+                    stops: [0, 100],
+                    opacityFrom: 0.4,
+                    opacityTo: 0.5,
+                  }
+                }
+              },
+              tooltip: {
+                enabled: true,
+              }
+            },
+            yaxis: {
+              axisBorder: {
+                show: false
+              },
+              axisTicks: {
+                show: false,
+              },
+              labels: {
+                show: false,
+                formatter: function (val) {
+                  return val + "%";
+                }
+              }
+            
+            },
+            title: {
+              text: 'Monthly Inflation in Argentina, 2002',
+              floating: true,
+              offsetY: 330,
+              align: 'center',
+              style: {
+                color: '#444'
+              }
+            }
+          },
+
+  })
+};
 </script>
 
-
-<style> 
-@import '../styles/cards.css';
+<style>
+@import "../styles/cards.css";
 </style>
