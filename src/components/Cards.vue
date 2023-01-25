f2a547bf23d49d8a7f9d84958376325ec7a33de7
Yaqoob <sualehfarooq65@gmail.com>
2023-01-25 14:51:09 +0500

    commit

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,21 +1,39 @@
 <template>
 <v-main >
-      <v-container>
-        <v-row>
-          <!-- <v-col
-            v-for="n in 20"
+      <v-container >
+        <div >
+        <v-row class="chart_upper_row"  >
+          <v-col
+            v-for="n in 1"
             :key="n"
-            cols="4" > -->
-            <v-card height="200" width="200">Card</v-card>
-            
-        </v-row>
+            cols="4" >
+            <v-card height="auto" width="auto"> <apexchart width="100%" height="100%" type="bar" :options="chartOptions" :series="series"></apexchart></v-card>
+             </v-col>
+            </v-row>
+          </div>
       </v-container>
     </v-main>
 </template>
 
 <script>
 export default {
-    name : 'CardsVue'
+    name : 'CompChrts',
+    data: function() {
+      return {
+        chartOptions: {
+          chart: {
+            id: 'vuechart-example'
+          },
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          }
+        },
+        series: [{
+          name: 'series-1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91]
+        }]
+      }
+    },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
 import sweetalert from './plugins/sweetalert'
 import VueApexCharts from 'vue-apexcharts'
 Vue.use(VueApexCharts)
-
+Vue.component('apexchart', VueApexCharts)
 
 Vue.config.productionTip = false
 
