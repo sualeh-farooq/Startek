9872ecb95b7250c27c447e0f9ec0c60680e0ea33
Yaqoob <sualehfarooq65@gmail.com>
2023-01-26 14:43:09 +0500

    commit

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,12 +1,9 @@
 <template>
-
-    <v-app>
+  <v-app>
     <v-row class="fill-height">
       <v-col>
         <v-sheet height="64">
-          <v-toolbar
-            flat
-          >
+          <v-toolbar flat>
             <v-btn
               outlined
               class="mr-4"
@@ -15,47 +12,21 @@
             >
               Today
             </v-btn>
-            <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="prev"
-            >
-              <v-icon small>
-                mdi-chevron-left
-              </v-icon>
+            <v-btn fab text small color="grey darken-2" @click="prev">
+              <v-icon small> mdi-chevron-left </v-icon>
             </v-btn>
-            <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="next"
-            >
-              <v-icon small>
-                mdi-chevron-right
-              </v-icon>
+            <v-btn fab text small color="grey darken-2" @click="next">
+              <v-icon small> mdi-chevron-right </v-icon>
             </v-btn>
             <v-toolbar-title v-if="$refs.calendar">
               {{ $refs.calendar.title }}
             </v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-menu
-              bottom
-              right
-            >
+            <v-menu bottom right>
               <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  outlined
-                  color="grey darken-2"
-                  v-bind="attrs"
-                  v-on="on"
-                >
+                <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
                   <span>{{ typeToLabel[type] }}</span>
-                  <v-icon right>
-                    mdi-menu-down
-                  </v-icon>
+                  <v-icon right> mdi-menu-down </v-icon>
                 </v-btn>
               </template>
               <v-list>
@@ -94,16 +65,9 @@
             :activator="selectedElement"
             offset-x
           >
-            <v-card
-              color="grey lighten-4"
-              min-width="350px"
-              flat
-            >
-              <v-toolbar
-                :color="selectedEvent.color"
-                dark
-              >
-                <v-btn icon>
+            <v-card color="grey lighten-4" min-width="350px" flat>
+              <v-toolbar :color="selectedEvent.color" dark>
+                <v-btn icon @click="editEvent">
                   <v-icon>mdi-pencil</v-icon>
                 </v-btn>
                 <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
@@ -119,11 +83,7 @@
                 <span v-html="selectedEvent.details"></span>
               </v-card-text>
               <v-card-actions>
-                <v-btn
-                  text
-                  color="secondary"
-                  @click="selectedOpen = false"
-                >
+                <v-btn text color="secondary" @click="selectedOpen = false">
                   Cancel
                 </v-btn>
               </v-card-actions>
@@ -132,92 +92,134 @@
         </v-sheet>
       </v-col>
     </v-row>
-</v-app>
-  </template>
-  
-  <script>
-    export default {
-      data: () => ({
-        focus: '',
-        type: 'month',
-        typeToLabel: {
-          month: 'Month',
-          week: 'Week',
-          day: 'Day',
-          '4day': '4 Days',
-        },
-        selectedEvent: {},
-        selectedElement: null,
-        selectedOpen: false,
-        events: [],
-        colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-        names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
-      }),
-      mounted () {
-        this.$refs.calendar.checkChange()
-      },
-      methods: {
-        viewDay ({ date }) {
-          this.focus = date
-          this.type = 'day'
-        },
-        getEventColor (event) {
-          return event.color
-        },
-        setToday () {
-          this.focus = ''
-        },
-        prev () {
-          this.$refs.calendar.prev()
-        },
-        next () {
-          this.$refs.calendar.next()
-        },
-        showEvent ({ nativeEvent, event }) {
-          const open = () => {
-            this.selectedEvent = event
-            this.selectedElement = nativeEvent.target
-            requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
-          }
-  
-          if (this.selectedOpen) {
-            this.selectedOpen = false
-            requestAnimationFrame(() => requestAnimationFrame(() => open()))
+  </v-app>
+</template>
+
+<script>
+import Swal from 'sweetalert2';
+export default {
+  data: () => ({
+    focus: "",
+    type: "month",
+    typeToLabel: {
+      month: "Month",
+      week: "Week",
+      day: "Day",
+      "4day": "4 Days",
+    },
+    selectedEvent: {},
+    selectedElement: null,
+    selectedOpen: false,
+    events: [],
+    colors: [
+      "blue",
+      "indigo",
+      "deep-purple",
+      "cyan",
+      "green",
+      "orange",
+      "grey darken-1",
+    ],
+    names: [
+      "Meeting",
+      "Holiday",
+      "PTO",
+      "Travel",
+      "Event",
+      "Birthday",
+      "Conference",
+      "Party",
+    ],
+  }),
+  mounted() {
+    this.$refs.calendar.checkChange();
+  },
+  methods: {
+    viewDay({ date }) {
+      this.focus = date;
+      this.type = "day";
+    },
+    getEventColor(event) {
+      return event.color;
+    },
+    setToday() {
+      this.focus = "";
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        requestAnimationFrame(() =>
+          requestAnimationFrame(() => (this.selectedOpen = true))
+        );
+      };
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        requestAnimationFrame(() => requestAnimationFrame(() => open()));
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
+    },
+    updateRange({ start, end }) {
+      const events = [];
+
+      const min = new Date(`${start.date}T00:00:00`);
+      const max = new Date(`${end.date}T23:59:59`);
+      const days = (max.getTime() - min.getTime()) / 86400000;
+      const eventCount = this.rnd(days, days + 20);
+
+      for (let i = 0; i < eventCount; i++) {
+        const allDay = this.rnd(0, 3) === 0;
+        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
+        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+        const second = new Date(first.getTime() + secondTimestamp);
+
+        events.push({
+          name: this.names[this.rnd(0, this.names.length - 1)],
+          start: first,
+          end: second,
+          color: this.colors[this.rnd(0, this.colors.length - 1)],
+          timed: !allDay,
+        });
+      }
+
+      this.events = events;
+    },
+
+    async editEvent() {
+      // alert('Working')
+      const { value: formValues } = await Swal.fire({
+        title: "Edit Event",
+        html: '<input id="swal-input1" class="swal2-input">',
+        focusConfirm: true,
+        preConfirm: () => {
+          if (document.getElementById("swal-input1").value.trim() === "") {
+            alert("Input Any Value");
           } else {
-            open()
+            console.log(document.getElementById("swal-input1").value);
+            const eventEdit = document.getElementById("swal-input1").value;
+            this.selectedEvent = eventEdit;
           }
-  
-          nativeEvent.stopPropagation()
         },
-        updateRange ({ start, end }) {
-          const events = []
-  
-          const min = new Date(`${start.date}T00:00:00`)
-          const max = new Date(`${end.date}T23:59:59`)
-          const days = (max.getTime() - min.getTime()) / 86400000
-          const eventCount = this.rnd(days, days + 20)
-  
-          for (let i = 0; i < eventCount; i++) {
-            const allDay = this.rnd(0, 3) === 0
-            const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-            const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-            const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-            const second = new Date(first.getTime() + secondTimestamp)
-  
-            events.push({
-              name: this.names[this.rnd(0, this.names.length - 1)],
-              start: first,
-              end: second,
-              color: this.colors[this.rnd(0, this.colors.length - 1)],
-              timed: !allDay,
-            })
-          }
-  
-          this.events = events
-        },
-        rnd (a, b) {
-          return Math.floor((b - a + 1) * Math.random()) + a
-        },
-      },
-    }
-  </script>
+      });
+
+      if (formValues) {
+        console.log(formValues);
+      }
+    },
+    rnd(a, b) {
+      return Math.floor((b - a + 1) * Math.random()) + a;
+    },
+  },
+};
+</script>
